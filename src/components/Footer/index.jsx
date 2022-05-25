15932de2897c066c0977e5de073c555a2e06b64a
Luan Henrique <65981953+LuanHMA@users.noreply.github.com>
2022-05-25 17:44:23 -0300

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ export function Footer(){
     return(
         <Container>
             <h1>Feito por <a href="https://github.com/LuanHMA" target="_blank">Luan Henrique</a></h1>
-            <h2>Interface feita pela Netflix</h2>
+            <h2>Design feito pela Netflix</h2>
             <h2>Dados obtidos da API do Tmdb</h2>
         </Container>
     )
